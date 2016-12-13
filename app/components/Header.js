190b064d1ea4c982f5b51a9a3445dd63787e0742
Bailey Miller <bailey@gaslight.co>
2016-12-13 17:02:17 -0500

    Move header to its own component

---
+++ app/components/Header.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Header = React.createClass({
+  render: function(){
+    return <h1 className='app-header'>This is the header </h1>;
+  }
+});
+
+module.exports = Header;
